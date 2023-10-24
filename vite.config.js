eac835d1129df42a3336b0c8f0a82433517de6a1
wwsheng009 <vincentwwsheng@gmail.com>
2023-10-24 08:16:31 +0800

    update index fetcher

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,42 @@ import { defineConfig, loadEnv } from "vite";
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
+  const username = process.env.VITE_SAP_USER;
+  const password = process.env.VITE_SAP_PASS;
   // https://vitejs.dev/config/
   return defineConfig({
     root: "./app",
     base: "./",
     server: {
-      host: "127.0.0.1",
-      port: 8888, // 端口
+      host: "localhost",
+      port: 9999, // 端口
       proxy: {
         "/sap": {
           target: "http://sapbp1809.e2yun.com:8080/", // 代理地址
           changeOrigin: true, // 是否允许跨域
           secure: false,
-          configure: (proxy, options) => {
-            // proxy will be an instance of 'http-proxy'
-            const username = process.env.USERNAME;
-            const password = process.env.PASSWORD;
-            options.auth = `${username}:${password}`;
-          },
+          auth: `${username}:${password}`,
+          // configure: (proxy, options) => {
+          //   console.log("process.env", process.env);
+          //   // proxy will be an instance of 'http-proxy'
+          //   const username = process.env.SAP_USER;
+          //   const password = process.env.SAP_PASS;
+          //   options.auth = `${username}:${password}`;
+          // },
         },
-        "/fmcall": {
+        "/amis": {
           // 请求接口中要替换的标识
           target: "http://sapbp1809.e2yun.com:8080/", // 代理地址
           changeOrigin: true, // 是否允许跨域
           secure: false,
-          configure: (proxy, options) => {
-            // proxy will be an instance of 'http-proxy'
-            const username = process.env.USERNAME;
-            const password = process.env.PASSWORD;
-            options.auth = `${username}:${password}`;
-          },
+          auth: `${username}:${password}`,
+          // configure: (proxy, options) => {
+          //   console.log("process.env", process.env);
+          //   // proxy will be an instance of 'http-proxy'
+          //   const username = process.env.SAP_USER;
+          //   const password = process.env.SAP_PASS;
+          //   options.auth = `${username}:${password}`;
+          // },
           // rewrite: (path) => path.replace(/^\/api/, ""), // api标志替换为''
         },
       },
